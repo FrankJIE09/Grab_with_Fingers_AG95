e212a03929272a9a77890cee211dc5c362dadebf
FrankJ <FrankJ@gmail.com>
2024-01-22 00:57:24 +0800

    a

--- UR_control/URControl.py
+++ UR_control/URControl.py
@@ -3,11 +3,10 @@
 # FTReading模块用于读取机器人的力矩传感器数据。
 # 该程序中定义了URControl类，其中包含机器人的IP地址、连接控制器、接收器、IO接口和力矩传感器读取接口等。get_robot()函数用于返回机器人本身，方便其他程序调用。
 # Created by Jie Yu
-import yaml
 import rtde_control
 import rtde_io
 import rtde_receive
-import FTReading
+
 from scipy.spatial.transform import Rotation as Rt
 
 
@@ -25,19 +24,11 @@ def rpy2rot_vec(rpy):
     return rot_vec
 
 
-def from_yaml_get_data(label):
-    file = open('../config/UR10e.yaml', 'r', encoding='utf-8')
-    read = file.read()
-    cfg = yaml.load(read, Loader=yaml.FullLoader)
-
-    return cfg[label]
-    pass
 
 
 class URControl:
     def __init__(self):
-        IP = from_yaml_get_data('ip')
-        FT_IP = from_yaml_get_data('FT_ip')
+        IP = '192.168.1.10'
         self.control_c = rtde_control.RTDEControlInterface(IP)
         self.receive_r = rtde_receive.RTDEReceiveInterface(IP)
         self.io_control = rtde_io.RTDEIOInterface(IP)
@@ -56,9 +47,9 @@ if __name__ == '__main__':
     r = Rt.from_rotvec(pose[3:6])
     print(rot_vec2rpy(pose))
     # tcp = rpy2rot_vec((-np.pi-np.pi/2, 0, 0))
-    tcp = rpy2rot_vec((-np.pi, 0, 0))
+    tcp = rpy2rot_vec((np.pi/2, -np.pi/2, np.pi/2))
     # -0.016139876756485903, -0.8102090933335253, 0.3722249608780756
-    init_pose = -0.0016820998616141562, -0.8697362302399543, 0.055184393030923595 +0.4
-
+    # init_pose = -0.10403260153599, -0.7062937886520618, 0.5050195419732867,
+    #
     # robot.control_c.moveL((init_pose[0], init_pose[1], init_pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
     #                       acceleration=0.1, asynchronous=False)
--- main.py
+++ main.py
@@ -1,3 +1,63 @@
-from Aruco_read.Finger_aruco_read import GetFingerForce
 from DaHuanFingerControl.ControlGripper import SetCmd
-from UR_control
+from DaHuanFingerControl.ControlRoot import ControlRoot
+from UR_control.URControl import URControl as Robot
+from UR_control.URControl import rpy2rot_vec
+import numpy as np
+import time
+
+robot = Robot()
+tcp = rpy2rot_vec((np.pi / 2, 0, np.pi / 2))
+tcp2 = rpy2rot_vec((np.pi / 2, -np.pi / 2, np.pi / 2))
+
+CR = ControlRoot()
+cs = SetCmd(CR)
+cs.HandInit()
+cs.InitFeedback()
+human_pose = [-0.25, -0.7562937886520618, 0.4550195419732867]
+
+
+def robot_moveL_with_static_rpy(pose, x, z, finger_pos):
+    robot.control_c.moveL((pose[0], pose[1], pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
+                          acceleration=0.1, asynchronous=False)
+    cs.Position(1000)
+    time.sleep(0.5)
+    robot.control_c.moveL((x, pose[1], pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
+                          acceleration=0.1, asynchronous=False)
+    cs.Position(finger_pos)
+    time.sleep(2)
+    robot.control_c.moveL((x, pose[1], z, tcp[0], tcp[1], tcp[2]), speed=0.03,
+                          acceleration=0.1, asynchronous=False)
+    robot.control_c.moveL((pose[0], pose[1], z, tcp[0], tcp[1], tcp[2]), speed=0.03,
+                          acceleration=0.1, asynchronous=False)
+    # cs.Position(1000)
+    robot.control_c.moveL((human_pose[0], human_pose[1], human_pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
+                          acceleration=0.1, asynchronous=False)
+    time.sleep(2)
+
+
+init_pose = [-0.18403260153599, -0.7062937886520618, 0.5050195419732867]
+
+robot.control_c.moveL((init_pose[0], init_pose[1], init_pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
+                      acceleration=0.1, asynchronous=False)
+x_init = -0.25
+pose1 = [x_init, -0.48381472961018573, 0.4587295304342077, ]
+# -0.06340571481115573, -0.48381472961018573, 0.4587295304342077,
+# -0.0866444379333002, -0.48381713660980097, 0.47898841330847836
+# pose = 200
+robot_moveL_with_static_rpy(pose1, x=-0.06340571481115573, z=0.47898841330847836, finger_pos=200)
+# exit()
+pose2 = [x_init, -0.6573053787678614, 0.6014474654541312]
+# -0.04708070453119846, -0.6573053787678614, 0.6014474654541312
+# [-0.04707429604645738, -0.6572898389610627, 0.649748684720083,
+# pose = 500
+robot_moveL_with_static_rpy(pose2, x=-0.04708070453119846, z=0.649748684720083, finger_pos=500)
+exit()
+
+pose3 = [-0.10403260153599, -0.7062937886520618, 0.5050195419732867 + 0.05, ]
+#
+robot_moveL_with_static_rpy(pose2)
+pose4 = [-0.10403260153599, -0.7062937886520618, 0.5050195419732867 + 0.05, ]
+
+
+robot.control_c.moveL((init_pose[0], init_pose[1], init_pose[2], tcp[0], tcp[1], tcp[2]), speed=0.03,
+                      acceleration=0.1, asynchronous=False)
